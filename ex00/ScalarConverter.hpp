85da597cda455e1b91e186e79cf94ddcc3396d1a
ridahafidi <ridahafidi5@gmail.com>
2026-02-03 21:35:25 +0100

    just starting off

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+#ifndef SCALAR_CONVERTER_HPP
+#define SCALAR_CONVERTER_HPP
+
+#include <iostream>
+#include <string>
+
+class ScalarConverter
+{
+    private:
+     ScalarConverter();
+    ScalarConverter(const ScalarConverter& other);   
+    ScalarConverter& operator=(const ScalarConverter& other);
+    ~ScalarConverter();
+public:
+    static void convert(const std::string &literal);
+};
+
+
+
+
+
+
+
+#endif
