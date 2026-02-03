371b85da8cdaab0e21c5bd8aaa3885634443a1fa
ridahafidi <ridahafidi5@gmail.com>
2026-02-03 21:48:33 +0100

    to be continued

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -7,13 +7,54 @@
 class ScalarConverter
 {
     private:
-     ScalarConverter();
+    ScalarConverter();
     ScalarConverter(const ScalarConverter& other);   
     ScalarConverter& operator=(const ScalarConverter& other);
     ~ScalarConverter();
 public:
     static void convert(const std::string &literal);
 };
+// "42.0f" "42"  "42f" '*'
+
+void ScalarConverter::convert(const std::string &literal)
+{
+    if (literal.empty() == true)
+    {
+        std::cerr << "The given input is empty";
+        return ;
+    }
+    int i = 0;
+    int f_counter = 0;
+    int dot_counter = 0;
+    int sign = 0;
+    int s_counter = 0;
+    bool is_char = false;
+    bool good = false;
+    while (literal[i])
+    {
+        if (isdigit(literal[i]) == false)
+        {
+            if (literal[i] == '+')
+            {
+                s_counter++;
+                sign = 1;
+            }
+            else if (literal[i] == '-')
+            {
+                s_counter++;
+                sign = -1;
+            }
+            else if (literal[i] == 'f')
+                f_counter++;
+            else if (literal[i] == '.')
+                dot_counter++;
+            else
+                good = false;
+        }
+        //handling char incoming
+        i++;
+    }
+}
 
 
 
